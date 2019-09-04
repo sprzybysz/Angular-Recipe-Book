df2a91873ea6ed50845d2be2a346b40a644acfcd
Przybysz <sabina.przybysz@capgemini.com>
2019-09-04 16:06:07 +0200

    add models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is test recipe',
+    'https://cdn.pixabay.com/photo/2017/09/19/20/41/pizza-2766568_960_720.jpg'),
+    new Recipe('Test recipe', 'This is test recipe',
+    'https://cdn.pixabay.com/photo/2017/09/19/20/41/pizza-2766568_960_720.jpg')
+  ];
 
   constructor() { }
 
